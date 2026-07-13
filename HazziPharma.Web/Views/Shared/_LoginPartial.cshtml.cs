e08cbece5eabfeee87e0d57551658c29551c6df0
moniruzzamanzilani <mzilani07@gmail.com>
2026-07-13 10:36:39 +0600

    Step 17.7: Authorization and login/logout completed

---
+++ HazziPharma.Web/Views/Shared/_LoginPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HazziPharma.Web.Views.Shared
+{
+    public class _LoginPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
