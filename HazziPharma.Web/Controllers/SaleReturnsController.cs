f89adf653388fab7070766ec50ddb7cd4f5b92a5
moniruzzamanzilani <mzilani07@gmail.com>
2026-07-20 16:10:36 +0600

    Complete Sale Return module with stock update

--- HazziPharma.Web/Controllers/SaleReturnsController.cs
+++ HazziPharma.Web/Controllers/SaleReturnsController.cs
@@ -1,4 +1,5 @@
 ﻿using HazziPharma.Web.Data;
+using HazziPharma.Web.Models;
 using HazziPharma.Web.Services;
 using HazziPharma.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -54,6 +55,84 @@ namespace HazziPharma.Web.Controllers
             });
 
             return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SaleReturnViewModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                model.ReturnNo = await _numberGenerator.GenerateSaleReturnNoAsync();
+
+                model.Sales = await _context.Sales
+                    .OrderByDescending(x => x.Id)
+                    .Select(x => new SelectListItem
+                    {
+                        Value = x.Id.ToString(),
+                        Text = x.SaleNo
+                    })
+                    .ToListAsync();
+
+                return View(model);
+            }
+
+            var saleReturn = new SaleReturn
+            {
+                ReturnNo = model.ReturnNo,
+                SaleId = model.SaleId,
+                ReturnDate = model.ReturnDate,
+                Remarks = model.Remarks,
+                TotalAmount = model.TotalAmount
+            };
+
+            _context.SaleReturns.Add(saleReturn);
+
+            await _context.SaveChangesAsync();
+
+            foreach (var item in model.Items)
+            {
+                if (item.Quantity <= 0)
+                    continue;
+
+                var detail = new SaleReturnDetail
+                {
+                    SaleReturnId = saleReturn.Id,
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity,
+                    ReturnPrice = item.ReturnPrice,
+                    SubTotal = item.SubTotal
+                };
+
+                _context.SaleReturnDetails.Add(detail);
+                var product = await _context.Products
+                     .FirstOrDefaultAsync(x => x.Id == item.ProductId);
+
+                if (product != null)
+                {
+                    product.Stock += item.Quantity;
+                }
+            }
+            await _context.SaveChangesAsync();
+
+            return Content("Sale Return Header Saved");
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetSaleItems(int saleId)
+        {
+            var items = await _context.SaleDetails
+                .Where(x => x.SaleId == saleId)
+                .Select(x => new
+                {
+                    ProductId = x.ProductId,
+                    ProductName = x.Product!.Name,
+                    SalePrice = x.SalePrice,
+                    Quantity = x.Quantity
+                })
+                .ToListAsync();
+
+            return Json(items);
         }
     }
 }
